6568b3dd65caf8b6c385e6745a3b6a759cd36e90
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-15 20:17:37 -0300

    Controle de fluxo com IF ELSE

--- Modulo01/src/ArrayListsController/ListaTarefas.java
+++ Modulo01/src/ArrayListsController/ListaTarefas.java
@@ -9,11 +9,23 @@ public class ListaTarefas {
     ArrayList<Tarefa> taferas = new ArrayList<>();
 
     public void adicionar(Tarefa tarefa){
-        this.taferas.add(tarefa);
+        if (tarefa != null && tarefa.obterTamanhoTarefa() <= 20){
+            this.taferas.add(tarefa);
+        }
+        else{
+            System.out.println("A tarefa é inválida.");
+            System.out.println("----------------------");
+        }
+
     }
 
     public void remover(int posicao){
-        this.taferas.remove(posicao);
+        if(posicao < this.taferas.size()){
+            this.taferas.remove(posicao);
+        }
+        else {
+            System.out.println("Tarefa não existe.");
+        }
     }
 
     public void exibirTarefas(){
--- Modulo01/src/ArrayListsController/PrincipalArray.java
+++ Modulo01/src/ArrayListsController/PrincipalArray.java
@@ -7,16 +7,28 @@ public class PrincipalArray {
     public static void main(String[] args) {
         System.out.println("Minhas Tarefas");
         System.out.println("-----------------------");
+
         ListaTarefas tarefas = new ListaTarefas();
+
         Tarefa tarefa1 = new Tarefa("Estudar Java");
         Tarefa tarefa2 = new Tarefa("Trabalhar");
         Tarefa tarefa3 = new Tarefa("Estudar para o Codenation");
+        Tarefa tarefa4 = new Tarefa("Estudar para dois AceleraDev ao mesmo tempo, seria possível?");
 
         tarefas.adicionar(tarefa1);
         tarefas.adicionar(tarefa2);
         tarefas.adicionar(tarefa3);
 
         tarefas.exibirTarefas();
+
+        tarefas.remover(4);
+
+        System.out.println("Minhas tarefas após remover:");
+        System.out.println("-----------------------");
+
+        tarefas.exibirTarefas();
+
+
 
 
     }
