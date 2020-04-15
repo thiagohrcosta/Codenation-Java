d15e4bf282114d02c0177b8f2101293de267ac35
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-15 18:06:42 -0300

    Realizando concatenação

--- Modulo01/src/br/com/Principal.java
+++ Modulo01/src/br/com/Principal.java
@@ -3,6 +3,9 @@ package br.com;
 public class Principal {
 
     public static void main(String[] args){
-        System.out.println("Hello Codenation!");
+
+        String nome = "Thiago";
+
+        System.out.println("Hello " + nome);
     }
 }
