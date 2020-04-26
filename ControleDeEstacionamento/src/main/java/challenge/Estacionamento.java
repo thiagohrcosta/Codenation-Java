9f15942eb72269b4faef3f0f370537ae472d8451
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-25 21:18:02 -0300

    Desafio Estacionamento - Refatorando e finalizando código

--- ControleDeEstacionamento/src/main/java/challenge/Estacionamento.java
+++ ControleDeEstacionamento/src/main/java/challenge/Estacionamento.java
@@ -6,27 +6,24 @@ public class Estacionamento {
 
     final LinkedList<Carro> vagasDisponiveis = new LinkedList<>();
 
+    private final static Integer IDADE_MINIMA = 18;
+    private final static Integer CNH_SUSPENSA = 20;
+    private final static Integer MOTORISTA_SENIOR = 56;
+    private final static Integer VAGAS_MAX_DISPONIVEIS = 10;
+
     public void estacionar(Carro carro) {
 
-        if(carro.getMotorista().getIdade() < 18){
-            throw new EstacionamentoException("Idade inferior ao mínimo legal");
-        }
-        else if(carro.getMotorista().getPontos() > 20 ){
-            throw new EstacionamentoException("Motorista com CNH suspensa.");
-        }
-        else if(carro.getMotorista().getPontos() < 0){
-            throw new EstacionamentoException("Motorista sem pontuação válida");
-        }
+        checkCarro(carro);
 
-        if(vagasDisponiveis.size() == 10){
+        if(vagasDisponiveis.size() == VAGAS_MAX_DISPONIVEIS){
             for(Carro carroEstacionado: vagasDisponiveis){
-                if(carroEstacionado.getMotorista().getIdade() < 56){
+                if(carroEstacionado.getMotorista().getIdade() < MOTORISTA_SENIOR){
                     vagasDisponiveis.removeFirstOccurrence(carroEstacionado);
                     break;
                 }
             }
         }
-        if(vagasDisponiveis.size() == 10){
+        if(vagasDisponiveis.size() == VAGAS_MAX_DISPONIVEIS){
             throw new EstacionamentoException("Todas as vagas prioritárias preenchidas.");
         }
         vagasDisponiveis.add(carro);
@@ -38,5 +35,20 @@ public class Estacionamento {
 
     public boolean carroEstacionado(Carro carro) {
         return vagasDisponiveis.contains(carro);
+    }
+
+    // Método de verificação
+
+    public boolean checkCarro(Carro carro){
+        if(carro.getMotorista().getIdade() < IDADE_MINIMA){
+            throw new EstacionamentoException("Idade inferior ao mínimo legal");
+        }
+        else if(carro.getMotorista().getPontos() > CNH_SUSPENSA ){
+            throw new EstacionamentoException("Motorista com CNH suspensa.");
+        }
+        else if(carro.getMotorista().getPontos() < 0){
+            throw new EstacionamentoException("Motorista sem pontuação válida");
+        }
+        return true;
     }
 }
